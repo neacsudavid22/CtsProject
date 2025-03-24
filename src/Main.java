58807b06f8e9c0a9071d1926b9687a256fe02839
neacsudavid22 <ndavid2003.dn@gmail.com>
2025-03-24 16:07:48 +0200

    tested ISingletonTonomatManager

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import cts.controllers.SingletonTonomatManager;
+import cts.enums.TipCompartiment;
+import cts.enums.TipProdus;
+import cts.interfaces.ISingletonTonomatManager;
+import cts.models.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("..Hello World!");
+        ISingletonTonomatManager tonomatManager = null;
+        tonomatManager = SingletonTonomatManager.getInstance();
+        Produs produs1 = new Produs(100, "coca cola", TipProdus.rece);
+        Produs produs2 = new Produs(90, "pepsi cola", TipProdus.rece);
+        Produs produs3 = new Produs(50, "idc", TipProdus.idc);
+        List<Produs> listaProduse = new ArrayList<>();
+        listaProduse.add(produs1); listaProduse.add(produs2); listaProduse.add(produs3);
+
+        Compartiment compartiment = new Compartiment(listaProduse, 4, TipCompartiment.produseReci);
+        tonomatManager.creazaTonomat(compartiment, 1000, "Bucuresti");
+        Tonomat tonomat = tonomatManager.selectezaTonomat(0);
+
+        System.out.println(tonomat.toString());
+        ContBancar contFirma = Tonomat.contFirma.getContBancar();
+        System.out.println(contFirma.toString());
     }
 }
