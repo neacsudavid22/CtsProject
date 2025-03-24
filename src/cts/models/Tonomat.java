371eaf674b75f5e2756f349430862706c61cf560
neacsudavid22 <ndavid2003.dn@gmail.com>
2025-03-24 16:05:50 +0200

    fixed SingletonContBancar initialization in Tonomat

--- src/cts/models/Tonomat.java
+++ src/cts/models/Tonomat.java
@@ -6,17 +6,13 @@ public class Tonomat {
     Compartiment compartiment;
     double sold;
     String locatie;
-    private static SingletonContBancar contFirma;
+    public static SingletonContBancar contFirma = SingletonContBancar.getInstance();
 
     public Tonomat(Compartiment compartiment, double sold, String locatie) {
         this.id = count++;
         this.compartiment = compartiment;
         this.sold = sold;
         this.locatie = locatie;
-    }
-
-    public static SingletonContBancar getContFirma() {
-        return contFirma;
     }
 
     public static int getCount() {
