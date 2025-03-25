79a86683bd0d99a160b12a26a08d2051ea799c3a
neacsudavid22 <ndavid2003.dn@gmail.com>
2025-03-25 17:31:27 +0200

    v1 of CLI

--- src/Main.java
+++ src/Main.java
@@ -11,9 +11,9 @@ public class Main {
     public static void main(String[] args) {
         ISingletonTonomatManager tonomatManager = null;
         tonomatManager = SingletonTonomatManager.getInstance();
-        Produs produs1 = new Produs(100, "coca cola", TipProdus.rece);
-        Produs produs2 = new Produs(90, "pepsi cola", TipProdus.rece);
-        Produs produs3 = new Produs(50, "idc", TipProdus.idc);
+        Produs produs1 = new Produs(100, "coca cola", "Coca cola srl", TipProdus.rece);
+        Produs produs2 = new Produs(90, "pepsi cola","pepsi cola srl", TipProdus.rece);
+        Produs produs3 = new Produs(50, "idc","Coca cola srl", TipProdus.idc);
         List<Produs> listaProduse = new ArrayList<>();
         listaProduse.add(produs1); listaProduse.add(produs2); listaProduse.add(produs3);
 
@@ -21,11 +21,6 @@ public class Main {
         tonomatManager.creazaTonomat(compartiment, 1000, "Bucuresti");
         Tonomat tonomat = tonomatManager.selectezaTonomat(0);
 
-        System.out.println(tonomat.toString());
-
-        ContBancar contFirma = Tonomat.contFirma.getContBancar();
-        System.out.println(contFirma.toString());
-
-        tonomatManager.listezaTonomatele();
+        tonomatManager.declanseazaCLI();
     }
 }
